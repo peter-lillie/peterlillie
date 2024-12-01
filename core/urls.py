b0ed9351e37f40a53dac8b04e922be3ab3d0edd2
Peter Lillie <sealchest@gmail.com>
2024-12-01 11:38:26 -0500

    got the basic urls and landing page up and running!

--- core/urls.py
+++ core/urls.py
@@ -15,11 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
-from django.urls import path, include
-from core import urls as core_urls
+from . import views as core_views
+from django.urls import path
 
 urlpatterns = [
-    path("admin/", admin.site.urls), 
-    path("", include(core_urls))
+    path("", view=core_views.landing_page, name="landing_page"),
     ]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
 # Create your views here.
+def landing_page(request: HttpRequest) -> HttpResponse:
+    return render(request, template_name="core/landing_page.html")
